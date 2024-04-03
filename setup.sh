cbc2927132f7bd08041342bd832862b9f280c522
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-03 08:05:20 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 git clone https://github.com/lllyasviel/Fooocus.git
-
 cd Fooocus
 
 conda env create -f environment.yaml
-
 eval "$(conda shell.bash hook)"
 conda activate fooocus
 
 pip install -r requirements_versions.txt
-
 pip install pyngrok
 conda install -y glib
 
