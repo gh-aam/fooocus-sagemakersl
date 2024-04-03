f8b4163636bc4a1087d34005f0888503d8b75b3a
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-03 08:12:45 +0600

    Update storage.sh

--- storage.sh
+++ storage.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+parent_dir=$(pwd)
 rm -rf ~/.cache/*
-rm -rf ~/fooocus-smsl/Fooocus/outputs/*
+rm -rf $parent_dir/Fooocus/outputs/*
 
 df -h | grep -E 'Avail|home'
 du -h --max-depth=1 ~
