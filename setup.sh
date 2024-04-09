2922e9e53309239c2ef05e899ad347851b8a6c2b
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-09 20:23:14 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,13 +6,11 @@ cd Fooocus
 conda env create -f environment.yaml
 eval "$(conda shell.bash hook)"
 conda activate fooocus
-
 pip install -r requirements_versions.txt
 pip install pyngrok
 conda install -y glib
+conda deactivate
 
 echo "{ \"path_checkpoints\": \"$(pwd)/models/checkpoints_data\" }" > config.txt
 mv models/checkpoints models/checkpoints_data
 ln -s models/checkpoints_data models/checkpoints
-
-conda deactivate
