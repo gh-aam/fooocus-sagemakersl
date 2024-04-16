b54c6fd8b86abec0b7fbad6fe9d3eb8962c862c0
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-16 23:42:24 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,3 @@ pip install -r requirements_versions.txt
 pip install pyngrok
 conda install -y glib
 conda deactivate
-
-echo "{ \"path_checkpoints\": \"$(pwd)/models/checkpoints_data\" }" > config.txt
-mv models/checkpoints models/checkpoints_data
-ln -s models/checkpoints_data models/checkpoints
