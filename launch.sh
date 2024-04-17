7cd39f96e41545cbe33888fbb46b8ca731597c46
Abdullah Al Mamun <m98.aam@gmail.com>
2024-04-18 00:18:28 +0600

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-cd Fooocus
-git pull
-cd ..
-
 eval "$(conda shell.bash hook)"
 conda activate fooocus
-if [ $# -eq 0 ]; then
-    python ngrok.py 
-elif [ $1 = "reset" ]; then
-    python ngrok.py --reset 
-fi
+python ngrok.py
